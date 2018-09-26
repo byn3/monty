cf7cd34a227dc25b9e1017e273d782766ceab562
byn3 <bryanleung92@gmail.com>
2018-09-26 19:48:45 +0000

    header function that includes all the prototypes

--- monty.h
+++ monty.h
@@ -6,7 +6,8 @@
 #include <stddef.h>
 #include <string.h>
 #include <math.h>
-#include <iostream.h>
+#include <unistd.h>
+#include <ctype.h>
 
 /**
 * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -39,3 +40,44 @@ typedef struct instruction_s
 } instruction_t;
 
 
+/**
+* struct global - the string, the arguments, the dataType, the file
+* @string: the string from the buffer
+* @argument: the arguments of the string
+* @dataType: will be used to differentiate stacks from queues
+* @file: the file we opened
+*
+* Description: global structure used to pass data around the functions easily
+* for stack, queues, LIFO, FIFO Holberton project
+*/
+typedef struct global
+{
+	char *gString;
+	char *argument;
+	int dataType;
+	FILE *file;
+} pitbull;
+
+extern pitbull world;
+pitbull world;
+
+void add(stack_t **stack, unsigned int lineCount);
+void freeAll(stack_t **h);
+int isDigglet(char *string);
+void lineUp(stack_t **stack, stack_t *temp);
+void nop(stack_t **stack, unsigned int lineCount);
+void opcode(stack_t **stack, char *string, unsigned int lineCount);
+void pall(stack_t **stack, unsigned int lineCount);
+void pint(stack_t **stack, unsigned int lineCount);
+void pop(stack_t **stack, unsigned int lineCount);
+void usageError(void);
+void fileReadError(char *argv);
+void badOpCode(stack_t **stack, char *string, unsigned int lineCount);
+void mallocError(stack_t **stack);
+void integerError(stack_t **stack, int lineCount);
+void push(stack_t **stack, unsigned int lineCount);
+void stackIt(stack_t **stack, stack_t *temp);
+void swap(stack_t **stack, unsigned int lineCount);
+
+
+#endif
