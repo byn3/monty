60006d4e32b4c23ddc1ee11488ef02bc12ec4c5f
byn3 <bryanleung92@gmail.com>
2018-09-26 19:51:10 +0000

    prints all thee elements untill stack is null

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+* pall - prints out all the pancakes
+* @stack: the stack of money im finna get
+* @lineCount: Used never cause it was voided
+*
+* Return - None it is void. lol everything gonna be void!
+*/
+
+void pall(stack_t **stack, unsigned int lineCount)
+{
+	(void) lineCount; /* Need this cause errors */
+	stack_t *temp = *stack; /* used a temp so stack isnt modified */
+
+	while (temp) /* loop through until NULL, printing everything */
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
