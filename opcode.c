74cfc0969283be7a4eec00c0f5632376c9048b70
byn3 <bryanleung92@gmail.com>
2018-09-26 19:50:55 +0000

    the pointer to structs. handles all the op codes

---
+++ opcode.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+* opcode - checks if the given string is an op code
+* @stack: the pancake that is passed around the table
+* @string: the argument or string or opcode
+* @lineCount: used for error message returns
+*
+* Return: none it is void!!
+*/
+
+
+void opcode(stack_t **stack, char *string, unsigned int lineCount)
+{
+	int i = 0;
+	instruction_t op[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	}; /* the opcodes we want to compare via string */
+
+	while (op[i].opcode)
+	{
+		if (strcmp(op[i].opcode, string) == 0)
+		{
+			op[i].f(stack, lineCount);
+			return; /* if we found a match, run the function */
+		}
+		i++;
+	}
+	/* nothing matched. BAD OP CODE! ERROR ERROR EXIT */
+	badOpCode(stack, string, lineCount);
+}
