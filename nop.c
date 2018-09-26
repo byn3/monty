525ed99a35eb8b78ef44c829cd2a249602ccdbf8
byn3 <bryanleung92@gmail.com>
2018-09-26 19:50:37 +0000

    it does nothing at all

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+* nop - lol this does nothing. why would we need this??
+* @stack: doesnt matter
+* @lineCount : insert emo quote. why even life?
+*
+* Return: NOTHING
+*/
+void nop(stack_t **stack, unsigned int lineCount)
+{
+	(void) stack;
+	(void) lineCount;
+}
